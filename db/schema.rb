5fb3eb01c5007cb5bfdcec0f24e1a9cad2459133
skua1101 <kazuya071101@gmail.com>
2022-03-11 06:50:02 +0000

    新規ログイン

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_10_054413) do
+ActiveRecord::Schema.define(version: 2022_03_11_060951) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -76,6 +76,14 @@ ActiveRecord::Schema.define(version: 2022_03_10_054413) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
+    t.string "last_name"
+    t.string "first_name"
+    t.string "last_name_kana"
+    t.string "first_name_kana"
+    t.string "post_code"
+    t.string "phone_number"
+    t.string "nationality"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
