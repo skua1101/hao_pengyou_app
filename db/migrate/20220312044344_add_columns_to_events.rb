860be8b7d3f1cf32b3fba1f8569d6371d095ba82
skua1101 <kazuya071101@gmail.com>
2022-03-12 05:02:13 +0000

    からむの追加

--- db/migrate/20220312044344_add_columns_to_events.rb
+++ db/migrate/20220312044344_add_columns_to_events.rb
@@ -2,5 +2,7 @@ class AddColumnsToEvents < ActiveRecord::Migration[6.1]
   def change
     add_column :events, :venue, :string
     add_column :events, :evet_address, :string
+    add_column :events, :event_address, :string
+    a
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_12_044344) do
+ActiveRecord::Schema.define(version: 2022_03_12_050110) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2022_03_12_044344) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "venue"
     t.string "evet_address"
+    t.string "event_address"
   end
 
   create_table "relationships", force: :cascade do |t|
