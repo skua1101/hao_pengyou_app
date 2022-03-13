ae27fe5e9f84e818c4e7ba1c20fedf4d1d310b6f
skua1101 <kazuya071101@gmail.com>
2022-03-13 17:02:09 +0000

    予約機能

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -6,8 +6,16 @@ class ReservationsController < ApplicationController
   end
 
   def confirm
+    @user = current_user
+    @event = Event.find(params[:id])
+  end
+
+  def create
+    @reservation = reservation.confirm
+    @reservation.save
   end
 
   def complete
+    @user =current_user
   end
 end
