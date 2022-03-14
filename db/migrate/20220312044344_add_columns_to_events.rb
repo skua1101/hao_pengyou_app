949dd588a60b0ad7c07dac897b9f69a58f02d206
skua1101 <kazuya071101@gmail.com>
2022-03-14 13:00:24 +0000

    [Fix] Migration

--- db/migrate/20220312044344_add_columns_to_events.rb
+++ db/migrate/20220312044344_add_columns_to_events.rb
@@ -1,8 +1,6 @@
 class AddColumnsToEvents < ActiveRecord::Migration[6.1]
   def change
     add_column :events, :venue, :string
-    add_column :events, :evet_address, :string
     add_column :events, :event_address, :string
-    a
   end
 end
