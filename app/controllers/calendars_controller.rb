990d7fc337fb133f1969cdc01c56bf38e1ac7c3d
skua1101 <kazuya071101@gmail.com>
2022-03-24 18:10:46 +0000

    [add]calendar

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -10,8 +10,29 @@ class CalendarsController < ApplicationController
 
   def create
     Calendar.create(calendar_params)
+    flash[:notice] = "予定を記録しました"
     redirect_to calendars_path
   end
+
+  def edit
+  end
+
+  def update
+    if @calendar.update(calendar_params)
+       flash[:notice] = "予定を更新しました"
+       redirect_to calendars_path
+    else
+       render :edit
+    end
+  end
+
+  def destroy
+    @calendar = Calendar.find(params[:id])
+    @calendar.destroy
+    flash[:notice] = "予定を削除しました"
+	  redirect_to calendars_path
+  end
+
 
   private
 
