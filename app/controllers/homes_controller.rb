3364b4c933bb57a3423ea80c1d2b45d53d195b36
skua1101 <kazuya071101@gmail.com>
2022-04-05 10:57:36 +0000

    [add]vision_api

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,6 @@
 class HomesController < ApplicationController
   def top
-    @events = Event.order(created_at: :desc).limit(8)
+    @events = Event.order(created_at: :desc).limit(4)
     @user = User.all
   end
 
