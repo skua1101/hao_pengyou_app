e960fb578fe4ccde041ed29986b0bb6e9caa71c1
skua1101 <kazuya071101@gmail.com>
2022-04-06 15:22:12 +0000

    [add]pf_image

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_one_attached :profile_image
   has_many :events
   has_many :reservations
   # フォロー機能
@@ -41,6 +43,14 @@ class User < ApplicationRecord
       notification = current_user.active_notifications.new(visited_id: id, action: 'follow')
       notification.save if notification.valid?
     end
+  end
+
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end
 end
 
